ff3e57ad60db12d83d07579773d05ab897332657
Showrin Rahman <99120088+showrin20@users.noreply.github.com>
2024-12-18 23:33:17 +0600

    Update and rename tri_star.py to triangle.py

---
+++ triangle.py
@@ -0,0 +1,32 @@
+n=6
+for i in range(n):
+    for j in range(0,i):
+        print("*",end=" ")
+    print()
+
+
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+
+
+
+
+n=6
+for i in range(n):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+
+
